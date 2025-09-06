a4f3da01fe19f8402bf7474ce1d15dd2e66e9a5d
Karolina Supruniuk <karolina.supruniuk@student.howest.be>
2025-09-06 20:36:01 +0200

    Route for retreiving a recipe with it's id

--- cookbook/routes/web.php
+++ cookbook/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RecipeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/recipes/{id}', [RecipeController::class, 'show']);
