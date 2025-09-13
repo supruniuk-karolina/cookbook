848353c9667cc89a6ddf3e40b53e333bd035fc90
Karolina Supruniuk <suprunukkarolina0@gmail.com>
2025-09-13 23:34:44 +0200

    some css rules + corrected the link to css in the blade layout

--- cookbook/resources/views/components/layout.blade.php
+++ cookbook/resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cookbook</title>
-    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/screen.css') }}">
 </head>
 <body class="bg-gray-100 font-sans">
     {{ $slot }}
