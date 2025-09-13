92ccb2523ee47e1006850acc4af0387b0f87c07e
Karolina Supruniuk <suprunukkarolina0@gmail.com>
2025-09-13 23:11:26 +0200

    I choose to go with vanilla CSS over Tailwind to make the learning curve smoother.

--- cookbook/resources/views/components/layout.blade.php
+++ cookbook/resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cookbook</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
 </head>
 <body class="bg-gray-100 font-sans">
     {{ $slot }}
--- cookbook/resources/views/recipes/show.blade.php
+++ cookbook/resources/views/recipes/show.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
-    <div class="max-w-2xl mx-auto py-6">
-        <h1 class="text-3xl font-bold mb-4">{{ $recipe->title }}</h1>
+    <div>
+        <h1>{{ $recipe->title }}</h1>
 
-        <div class="prose">
+        <div>
             {!! $content !!}
         </div>
     </div>
