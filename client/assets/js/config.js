84ab651367f50ee6d54fa21a2b0e1572585c45ff
Karolina Supruniuk <suprunukkarolina0@gmail.com>
2025-09-17 00:48:37 +0200

    Remove the token configuration for the current stage of development, as I now want to satisfy only the MVP scope, and the post requests are not necessary for that.

--- client/assets/js/config.js
+++ client/assets/js/config.js
@@ -1,15 +1,10 @@
 const config = {
     baseApiUrl: 'https://server.test/api/',
     recipesEndpoint: 'recipes/',
-    token: undefined //TODO!!
 };
 
 function getRecipesUrl(){
     return config.baseApiUrl + config.recipesEndpoint;
 }
 
-function getToken(){
-    return config.token;
-}
-
-export { getRecipesUrl, getToken };
+export { getRecipesUrl };
---
+++ client/assets/js/recipes-component/fetcher.js
@@ -0,0 +1,1 @@
+import { getRecipesUrl } from "../config.js";
